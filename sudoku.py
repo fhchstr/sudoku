126736fbe978b2f81901b05e819813f9b92bed81
Fabien Hochstrasser <fabien.hochstrasser@swisscom.com>
2018-12-29 11:32:39 +0100

    Refactor and improve resolution performance.
    1. Remove the get/(un)set methods
       They should not be used in the bruteforcing process because parameter
       validation reduces the performance and is not needed
    
    2. Compute a few additional things at object's initialization
       This avoids to compute them thousands of times during the brute force
    
    3. Improve the correct() method performance
    
    4. Remove other unnecessary methods
    
    5. Refactor the solve() method to be a global function
    
    6. Prepare the code to handle sudokus of any size

--- sudoku.py
+++ sudoku.py
@@ -3,150 +3,106 @@ import math
 class Sudoku(object):
 
     def __init__(self, board):
-        self.board_size = len(board)
-        if not math.sqrt(self.board_size).is_integer():
+        if not math.sqrt(len(board)).is_integer():
             raise MalformedBoard('The square root of the board\'s size must be an integer')
 
-        self.unmodifiable_cells = set()
+        self.board_size = len(board)
+        self.values_per_square = int(math.sqrt(self.board_size))
         self.board = [[None] * self.board_size for _ in xrange(self.board_size)]
+        # Coordinates of the cells which contain a value during the object's instantiation
+        self.unmodifiable_cells = set()
 
+        # Make sure the board is valid and initialize the instance variables with its content
         for row in xrange(self.board_size):
             if not len(board[row]) == self.board_size:
                 raise MalformedBoard('The board must be a square ({0}x{0})'.format(self.board_size))
             for column in xrange(self.board_size):
                 value = board[row][column]
-                self.set(value, row, column)
                 if value:
+                    if value not in self.valid_values():
+                        raise ValueError('{} is not a valid value'.format(value))
                     self.unmodifiable_cells.add((row, column))
+                self.board[row][column] = value
 
-    def get(self, row, column):
-        """Get the value at the given coordinates."""
-        return self.board[row][column]
+        # Those coordinates are computed at object's initilization because the correct() method,
+        # which is called thousands of times during the brute-force resolution of the sudoku, needs it
+        self.top_left_corner_of_squares = set()
+        square_steps = range(0, self.board_size, self.values_per_square)
+        for square_top_row in square_steps:
+            for square_left_column in square_steps:
+                self.top_left_corner_of_squares.add((square_top_row, square_left_column))
 
-    def set(self, value, row, column):
-        """Set the given value at the given coordinates."""
-        if not self.modifiable(row, column):
-            raise UnmodifiableCell('The cell {}:{} is not modifiable'.format(row, column))
-
-        cell_value = None
-        if value is not None:
-            cell_value = int(value)
-            if not 1 <= cell_value <= self.board_size:
-                raise ValueError('The board values must be from 1 to {}'.format(self.board_size))
-
-        self.board[row][column] = cell_value
-
-    def unset(self, row, column):
-        """Set the value to None at the given coordinates."""
-        self.set(None, row, column)
+    def valid_values(self):
+        """Return the values allowed given the board size."""
+        return {v for v in xrange(1, self.board_size + 1)}
 
     def modifiable(self, row, column):
         """Return True if the cell at the given coordinates is modifiable."""
         return (row, column) not in self.unmodifiable_cells
 
-    # FIXME huge improvement potential
-    # instead of checking each cell, we should check each row, column and square. that's it.
-    # This also eliminates the need for the _coordinates() method
     def correct(self):
         """Return True if the Sudoku is correct."""
-        # Check first if all the rows are filled
-        for row, column in self._coordinates():
-            if not self.get(row, column):
+        # Return False if any row/column doesn't contain all the values
+        for i in xrange(self.board_size):
+            row_values = set(self.board[i])
+            column_values = {self.board[row][i] for row in xrange(self.board_size)}
+            if not row_values == column_values == self.valid_values():
                 return False
 
-        for row, column in self._coordinates():
-            value = self.get(row, column)
-
-            # Check the values on the same row/column
-            for i in xrange(self.board_size):
-                if i != column and self.get(row, i) == value:
-                    return False
-                if i != row and self.get(i, column) == value:
-                    return False
-
-        # Check the values in the same square
-        for r, c in self.square_coordinates(row, column):
-            if r != row and c != column and self.get(r, c) == value:
+        # Return False if any square doesn't contain all the values
+        for top_left_corner in self.top_left_corner_of_squares:
+            square_values = {self.board[r][c] for r, c in self.square_coordinates(*top_left_corner)}
+            if square_values != self.valid_values():
                 return False
 
         return True
 
     def square_coordinates(self, row, column):
         """Return the coordinates of the cells in the square in which the given coordinates are."""
-        square_size = int(math.sqrt(self.board_size))
-        r = row // square_size * square_size
-        c = column // square_size * square_size
+        top_row = row // self.values_per_square * self.values_per_square
+        left_column = column // self.values_per_square * self.values_per_square
 
-        for square_row in xrange(r, r + square_size):
-             for square_column in xrange(c, c + square_size):
-                 yield square_row, square_column
-
-    def next_coordinates(self, row, column):
-        if column < self.board_size - 1:
-            return row, column + 1
-        elif row < self.board_size - 1:
-            return row + 1, 0
-
-        return None, None
-
-    def solve(self):
-        """Return a new solved Sudoku instance."""
-        return Sudoku._solve_recursive(Sudoku(self.board))
-
-    @staticmethod
-    def _solve_recursive(sudoku, row=0, column=0):
-        if sudoku.correct():
-            return sudoku
-
-        next_row, next_column = sudoku.next_coordinates(row, column)
-
-        if not sudoku.modifiable(row, column):
-            # This cell is not modifiable, go to the next one
-            return Sudoku._solve_recursive(sudoku, next_row, next_column)
-
-        for possibility in sudoku.possibilities(row, column):
-            # Try each possibility and go to the next cell
-            sudoku.set(possibility, row, column)
-
-            finished = Sudoku._solve_recursive(sudoku, next_row, next_column)
-            if finished:
-                # Return the solution if we found it
-                return finished
-            else:
-                # Discard this possibility
-                sudoku.unset(row, column)
+        for row_in_square in xrange(top_row, top_row + self.values_per_square):
+             for column_in_square in xrange(left_column, left_column + self.values_per_square):
+                 yield row_in_square, column_in_square
 
     def possibilities(self, row, column):
         """Return the valid possibilities for a given coordinate."""
-        p = {x for x in xrange(1, self.board_size + 1)}
+        p = self.valid_values()
 
         for i in xrange(self.board_size):
             if i != column:
-                p.discard(self.get(row, i))
+                p.discard(self.board[row][i])
             if i != row:
-                p.discard(self.get(i, column))
+                p.discard(self.board[i][column])
+
         for r, c in self.square_coordinates(row, column):
             if r != row and c != column:
-                p.discard(self.get(r, c))
+                p.discard(self.board[r][c])
+
         return p
 
-    def _coordinates(self):
-        """Generator over the coordinates of the Sudoku board."""
-        for row in xrange(self.board_size):
-            for column in xrange(self.board_size):
-                yield row, column
-
     def __str__(self):
-        square_size = int(math.sqrt(self.board_size))
         lines = []
-        horizontal_line = '+' + '+'.join(['-' * (2 * square_size + 1)] * square_size) + '+'
+        # Find the largest width of the values's string representation
+        value_width = max([len(str(v)) for v in self.valid_values()])
+        # Space between the square border and the values (1 on each side) -----------------+
+        # Space between the values ----------------------------------------+               |
+        # Space to display the values --+                                  |               |
+        #              +----------------+-----------------+   +------------+-----------+   |
+        #              |                                  |   |                        |   |
+        square_width = self.values_per_square * value_width + self.values_per_square - 1 + 2
+        # A horizontal line is composed of lines made of dashes ('-') the same width as the squares
+        # Between each square, the dash is replaced by a plus sign ('+')
+        # The number of squares on a row is the same as the number of values in a square
+        horizontal_line = '+' + '+'.join(['-' * square_width] * self.values_per_square) + '+'
         for row_index, row in enumerate(self.board):
-            if row_index % square_size == 0:
+            if row_index % self.values_per_square == 0:
                 lines.append(horizontal_line)
 
             values = []
             for column_index, cell in enumerate(row):
-                if column_index % square_size == 0:
+                if column_index % self.values_per_square == 0:
                     values.append('|')
                 values.append(str(cell) if cell else ' ')
 
@@ -157,8 +113,48 @@ class Sudoku(object):
         return '\n'.join(lines)
 
 
+def solve(sudoku):
+    """Return a new solved Sudoku instance. Return None if no solution exist."""
+    def solve_recursive(sudoku, row=0, column=0):
+        if sudoku.correct():
+            return sudoku
+
+        # We tried everything and couldn't solve the sudoku
+        if row is None or column is None:
+            return None
+
+        # Compute the next coordinate
+        next_row, next_column = row, column
+        if column < sudoku.board_size - 1:
+            next_column +=  1
+        elif row < sudoku.board_size - 1:
+            next_row += 1
+            next_column = 0
+
+        if not sudoku.modifiable(row, column):
+            # This cell is not modifiable, go to the next one
+            return solve_recursive(sudoku, next_row, next_column)
+
+        for possibility in sudoku.possibilities(row, column):
+            # Try each possibility and go to the next cell
+            sudoku.board[row][column] = possibility
+            result = solve_recursive(sudoku, next_row, next_column)
+
+            if result:
+                # Return the solution if we found it
+                return result
+            else:
+                # Clear the cell to make sure the next attempt doesn't take it into account
+                sudoku.board[row][column] = None
+
+    return solve_recursive(Sudoku(sudoku.board))
+
+
 class MalformedBoard(Exception):
     pass
 
 class UnmodifiableCell(Exception):
+    pass
+
+class InvalidCell(Exception):
     pass
--- test/test_sudoku.py
+++ test/test_sudoku.py
@@ -59,6 +59,20 @@ EVIL = [
     [5,    None, 3,       None, None, 1,       None, None, None],
 ]
 
+HARDEST = [
+    [8,    None, None,    None, None, None,    None, None, None],
+    [None, None, 3,       6,    None, None,    None, None, None],
+    [None, 7,    None,    None, 9,    None,    2,    None, None],
+
+    [None, 5,    None,    None, None, 7,       None, None, None],
+    [None, None, None,    None, 4,    5,       7,    None, None],
+    [None, None, None,    1,    None, None,    None, 3,    None],
+
+    [None, None, 1,       None, None, None,    None, 6,    8   ],
+    [None, None, 8,       5,    None, None,    None, 1,    None],
+    [None, 9,    None,    None, None, None,    4,    None, None],
+]
+
 
 CORRECT = [
     [8, 3, 2,   1, 5, 9,   4, 7, 6],
@@ -88,48 +102,36 @@ INCORRECT = [
     [2, 1, 8,   5, 6, 7,   9, 4, 3],
 ]
 
+IMPOSSIBLE = [
+    [8,    3,    None,    1,    5,    9,       4,    7,    6],
+    [5,    4,    6,       2,    7,    8,       3,    9,    1],
+    [9,    7,    1,       3,    4,    6,       5,    8,    2],
+
+    [1,    8,    3,       6,    9,    5,       7,    2,    4],
+    [7,    5,    9,       4,    2,    1,       6,    3,    8],
+    [6,    None, 2,       7,    8,    3,       1,    5,    9],
+
+    [3,    9,    7,       8,    1,    4,       2,    6,    5],
+    [4,    6,    5,       9,    3,    2,       8,    1,    7],
+    [2,    1,    8,       5,    6,    7,       9,    4,    3],
+]
+
+
 
 class TestSudoku(unittest.TestCase):
 
     def setUp(self):
+        self.hardest = Sudoku(HARDEST)
         self.easy = Sudoku(EASY)
         self.medium = Sudoku(MEDIUM)
         self.hard = Sudoku(HARD)
         self.evil = Sudoku(EVIL)
         self.correct = Sudoku(CORRECT)
         self.incorrect = Sudoku(INCORRECT)
+        self.impossible = Sudoku(IMPOSSIBLE)
 
     # __init__
     # TODO
-
-    # get
-    def test_get_unset(self):
-        self.assertIs(self.easy.get(0, 0), None)
-
-    def test_get_value(self):
-        self.assertEqual(self.easy.get(0, 1), 3)
-
-    # set
-    def test_set_valid(self):
-        self.easy.set(1, 0, 0)
-        self.assertEqual(self.easy.get(0, 0), 1)
-
-    def test_set_invalid(self):
-        with self.assertRaises(ValueError):
-            self.easy.set(0, 0, 0)
-
-    def test_set_on_unmodifiable(self):
-        with self.assertRaises(sudoku.UnmodifiableCell):
-            self.easy.set(1, 0, 1)
-
-    # unset
-    def test_unset_valid(self):
-        self.easy.unset(0, 0)
-        self.assertIs(self.easy.get(0, 0), None)
-
-    def test_unset_on_unmodifiable(self):
-        with self.assertRaises(sudoku.UnmodifiableCell):
-            self.easy.unset(0, 1)
 
     # modifiable
     def test_modifiable_true(self):
@@ -169,23 +171,6 @@ class TestSudoku(unittest.TestCase):
             ]
         )
 
-    # solve
-    def test_solve_easy(self):
-        solved = self.easy.solve() 
-        self.assertTrue(solved.correct())
-
-    def test_solve_medium(self):
-        solved = self.medium.solve() 
-        self.assertTrue(solved.correct())
-
-    def test_solve_hard(self):
-        solved = self.hard.solve() 
-        self.assertTrue(solved.correct())
-
-    def test_solve_evil(self):
-        solved = self.evil.solve() 
-        self.assertTrue(solved.correct())
-
     # possibilities
     def test_single_possibility(self):
         self.assertEqual(self.correct.possibilities(0, 0), set([8]))
@@ -198,3 +183,27 @@ class TestSudoku(unittest.TestCase):
 
     # __str__
     # TODO
+
+    # solve
+    def test_solve_easy(self):
+        solved = sudoku.solve(self.easy)
+        self.assertTrue(solved.correct())
+
+    def test_solve_medium(self):
+        solved = sudoku.solve(self.medium)
+        self.assertTrue(solved.correct())
+
+    def test_solve_hard(self):
+        solved = sudoku.solve(self.hard)
+        self.assertTrue(solved.correct())
+
+    def test_solve_evil(self):
+        solved = sudoku.solve(self.evil)
+        self.assertTrue(solved.correct())
+
+    def test_solve_hardest(self):
+        solved = sudoku.solve(self.hardest)
+        self.assertTrue(solved.correct())
+
+    def test_solve_impossible(self):
+        self.assertIsNone(sudoku.solve(self.impossible))
